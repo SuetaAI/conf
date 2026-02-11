75048387a2356443f8d4cfd7bee833eb4342e964
SuetaAI <evgendushniy@gmail.com>
2026-02-11 17:38:37 +0300

    Enhance configuration with Bluetooth and Sound settings
    Added configurations for Bluetooth, Sound, and updated system packages.

--- configuration.nix
+++ configuration.nix
@@ -1,32 +1,50 @@
 { config, pkgs, inputs, ... }: {
   imports = [ ./hardware-configuration.nix ];
 
-  # Bootloader settings
+  # 1. Bootloader and Kernel
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_latest; # Required for SCX
 
-  # Networking
+  # 2. Networking
   networking.hostName = "my-machine";
   networking.networkmanager.enable = true;
 
-  # User setup
+  # 3. Graphics (This fix is for libgbm/mesa)
+  hardware.graphics.enable = true;
+
+  # 4. Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  # 5. Sound (Pipewire)
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+
+  # 6. User Account
   users.users.user = {
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" "video" ];
   };
 
-  # Niri and experimental features
+  # 7. Niri Configuration
   programs.niri.enable = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  services.displayManager.sddm.enable = false; # Manual TTY login
 
+  # 8. Packages
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   environment.systemPackages = with pkgs; [
-    vim wget git alacritty firefox scx
+    vim wget git alacritty firefox
+    mesa # Essential for GBM/Niri
   ];
 
-  # Sched-ext settings
+  # 9. SCX Scheduler
   services.scx.enable = true;
   services.scx.scheduler = "scx_rustland";
 
-  system.stateVersion = "24.11";
+  system.stateVersion = "24.11"; #
 }
